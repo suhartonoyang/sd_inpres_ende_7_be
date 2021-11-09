d938fb15434bb45a7cb8ecfdf8b00f243677962a
suhartonoyang <suhartonoyang@gmail.com>
2021-11-09 11:38:17 +0700

    initial project

---
+++ src/main/java/co/id/sdinpresende7be/SdInpresEnde7BeApplication.java
@@ -0,0 +1,13 @@
+package co.id.sdinpresende7be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdInpresEnde7BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdInpresEnde7BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/sdinpresende7be/SdInpresEnde7BeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.sdinpresende7be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdInpresEnde7BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
